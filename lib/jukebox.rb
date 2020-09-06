04a39ec6886f5c046fa6bcff8617ca248aa6b6db
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-06 00:29:00 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,30 @@
-# Add your code here
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.strip
+ 
+# puts say_hello(users_name)
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+   
+end
+
+def list(songs)
+  index = 0
+  while index < songs.length do
+    puts "#{index + 1}. #{songs[index]}"
+    index += 1
+  end
+end
