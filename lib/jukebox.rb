e1981b4000ded87120c69111cbd0916960cf7fd2
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-06 00:58:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,8 +20,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   list = songs_list_helper(songs)
-  binding.pry
   list.each do |number, name|
+    binding.pry
     if input == number.to_s || input == name
       puts "Playing #{name}"
     else
