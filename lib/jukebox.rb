be4b51c53403f021919fb14c1a2e11b4027733ca
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-06 01:35:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,10 +12,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   songs.each_with_index do |song, index|
-    if input == (index + 1).to_s
-      puts "Playing #{song}"
-    elsif input == song
-      puts "Playing #{song}"
+    if input == (index + 1).to_s || input == song
+      puts "Playing #{track[:song]}"
     else
       puts "Invalid input, please try again"
     end
