b18d5917905a288532ee6f1781dcd98203874e57
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-06 00:48:47 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  songs_list_helper(songs)
+  list = songs_list_helper(songs)
+  binding.pry
 end
 
 def list(songs)
