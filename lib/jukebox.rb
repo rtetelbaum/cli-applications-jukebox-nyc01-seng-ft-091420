0d8f512d3a0756560773ff83b6e1f63cfbf2ffae
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-06 02:04:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,8 +34,8 @@ end
 def run(songs)
 puts "Please enter a command:"
   input = gets.strip
-  while input != "exit" do
   exit_jukebox if input == "exit"
+  while input != "exit" do
   help if input == "help"
   list(songs) if input == "list"
   play(songs) if input == "play"
